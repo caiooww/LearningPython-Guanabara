31b66811b1007136c1baaf4ca6b60c1ac27115e2
caiow <caiomksp@gmail.com>
2024-08-20 16:26:28 -0700

    add 002 exercise

---
+++ Exercises/ex002.py
@@ -0,0 +1,8 @@
+#Create a program that read the name of the people and says a welcome message
+
+name = input("What is your name? ")
+print("Welcome, " + name)
+
+#or
+
+print("Welcome, {}!!".format(name))
