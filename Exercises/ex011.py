180ee821f6783be4f5d77cc82a9d7f0625bf87a7
caiow <caiomksp@gmail.com>
2024-08-21 13:22:46 -0700

    att 2nd print of the ex011

--- Exercises/ex011.py
+++ Exercises/ex011.py
@@ -6,4 +6,5 @@ height = float(input("Enter the height of the wall: "))
 area = width * height
 paint = area / 2
 
+print(f"The area of the wall is {area:.2f} m^2")
 print(f"You will need {paint:.2f} liters of paint")
